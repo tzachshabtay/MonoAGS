f74bb7251571fe04d2b9945d3d673fa819590def
Tzach Shabtay <tzachshabtay@gmail.com>
2015-09-15 18:53:12 -0400

    Added DebugLabel
    DebugLabel is a convenient way to track a value on the screen. It
    receives a function that returns a string and calls it every tick and
    show the result on screen.

---
+++ Engine/UI/DebugLabels/DebugLabel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using AGS.API;
+
+namespace AGS.Engine
+{
+	public class DebugLabel
+	{
+		private ILabel _label;
+		private IGame _game;
+		private Func<IGame, string> _getText;
+
+		public DebugLabel(IGame game, Func<IGame, string> getText, ILabel label)
+		{
+			_game = game;
+			_getText = getText;
+			_label = label;
+		}
+
+		public void Start()
+		{
+			_game.Events.OnRepeatedlyExecute.Subscribe(onTick);
+		}
+
+		private void onTick(object sender, EventArgs args)
+		{
+			try
+			{
+				_label.Text = _getText(_game);
+			}
+			catch (Exception e)
+			{
+				_label.Text = e.Message;
+			}
+		}
+	}
+}
+
