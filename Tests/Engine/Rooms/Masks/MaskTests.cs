74cb855c93f22dac1dd465ae3bd1cff603a8d403
tzachshabtay <tzachshabtay@gmail.com>
2015-09-18 15:44:50 -0400

    Added unit tests

--- Tests/Engine/Rooms/Masks/MaskTests.cs
+++ Tests/Engine/Rooms/Masks/MaskTests.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using NUnit.Framework;
 using AGS.Engine;
+using Moq;
+using AGS.API;
 
 namespace Tests
 {
@@ -19,6 +21,116 @@ namespace Tests
 			int expectedMinX, int expectedMaxX, int expectedMinY, int expectedMaxY,
 			params int[] pointsInMask)
 		{
+			bool[][] array = getArray(width, height, pointsInMask);
+
+			AGSMask mask = new AGSMask(array, null);
+
+			Assert.AreEqual(expectedMinX, mask.MinX);
+			Assert.AreEqual(expectedMaxX, mask.MaxX);
+			Assert.AreEqual(expectedMinY, mask.MinY);
+			Assert.AreEqual(expectedMaxY, mask.MaxY);
+		}
+
+		//Out of bounds: minimum
+		[TestCase(100,100, -1f,5f, Result=false)]
+		[TestCase(100,100, 5f,-1f, Result=false)]
+		[TestCase(100,100, -1f,5f, 1,5, Result=false)]
+		[TestCase(100,100, 5f,-1f, 5,1, Result=false)]
+
+		//Out of bounds: maximum
+		[TestCase(100,100, 100f,5f, Result=false)]
+		[TestCase(100,100, 5f,100f, Result=false)]
+		[TestCase(100,100, 100f,5f, 99,5, Result=false)]
+		[TestCase(100,100, 5f,100f, 5,99, Result=false)]
+
+		//Not masked
+		[TestCase(100,100, 1f,1f, Result=false)]
+		[TestCase(100,100, 1f,1f, 2,2, Result=false)]
+		[TestCase(100,100, 1f,1f, 0,0, 2,2, Result=false)]
+
+		//Masked
+		[TestCase(100,100, 1f,1f, 1,1, Result=true)]
+		[TestCase(100,100, 13.2f,5f, 0,0, 13,5, Result=true)]
+		public bool IsMaskedTest(int width, int height, float x, float y, params int[] pointsInMask)
+		{
+			bool[][] array = getArray(width, height, pointsInMask);
+			AGSMask mask = new AGSMask(array, null);
+			return mask.IsMasked(new AGSPoint (x, y));
+		}
+
+		//Not masked
+		[TestCase(100,100, 1f,1f, 0f,0f,1f,1f, Result=false)]
+		[TestCase(100,100, 1f,1f, 0f,0f,1f,1f, 2,2, Result=false)]
+		[TestCase(100,100, 1f,1f, 0f,0f,1f,1f, 0,0, 2,2, Result=false)]
+
+		[TestCase(100,100, 1f,1f, 5f,10f,1f,1f, 2,2, Result=false)]
+		[TestCase(100,100, 1f,1f, 5f,10f,1f,1f, 0,0, 2,2, Result=false)]
+		[TestCase(100,100, 5f,10f, 5f,10f,1f,1f, 5,10, 2,2, Result=false)]
+		[TestCase(100,100, 5f,10f, 5f,10f,3f,3f, 5,10, 2,2, Result=false)]
+		[TestCase(100,100, 5f,10f, 5f,10f,3f,4f, 5,10, 2,2, Result=false)]
+
+		//Masked
+		[TestCase(100,100, 1f,1f, 0f,0f,1f,1f, 1,1, Result=true)]
+		[TestCase(100,100, 13.2f,5f, 0f,0f,1f,1f, 0,0, 13,5, Result=true)]
+
+		[TestCase(100,100, 5f,10f, 5f,10f,3f,3f, 0,0, Result=true)]
+		[TestCase(100,100, 5f,10f, 5f,10f,1f,1f, 0,0, Result=true)]
+		[TestCase(100,100, 5f,10f, 5f,10f,3f,4f, 0,0, Result=true)]
+
+		[TestCase(100,100, 16f,34f, 15f,33f,1f,1f, 1,1, Result=true)]
+		[TestCase(100,100, 19f,37f, 15f,33f,4f,4f, 1,1, Result=true)]
+
+		[TestCase(100,100, 150f,400f, 100f,200f,10f,20f, 5,10, Result=true)]
+		[TestCase(100,100, 150.4f,400.4f, 100f,200f,10f,20f, 5,10, Result=true)]
+
+		//Negative Scale
+		[TestCase(100,100, 16f,34f, 15f,33f,-1f,1f, 1,1, Result=false)]
+		[TestCase(100,100, 16f,34f, 15f,33f,1f,-1f, 1,1, Result=false)]
+		[TestCase(100,100, 16f,34f, 15f,33f,-1f,1f, 98,1, Result=true)]
+		[TestCase(100,100, 16f,34f, 15f,33f,1f,-1f, 1,98, Result=true)]
+		public bool IsMasked_WithProjection_Test(int width, int height, float x, float y, float projectionLeft, float projectionBottom, float scaleX, float scaleY, params int[] pointsInMask)
+		{
+			bool[][] array = getArray(width, height, pointsInMask);
+			AGSMask mask = new AGSMask(array, null);
+			Mock<ISquare> square = new Mock<ISquare> ();
+
+			AGSPoint bottomLeft = new AGSPoint (projectionLeft, projectionBottom);
+			AGSPoint bottomRight = new AGSPoint (projectionLeft + width * Math.Abs(scaleX), projectionBottom);
+			AGSPoint topLeft = new AGSPoint (projectionLeft, projectionBottom + height * Math.Abs(scaleY));
+			AGSPoint topRight = new AGSPoint (projectionLeft + width * Math.Abs(scaleX), projectionBottom + height * Math.Abs(scaleY));
+			square.Setup(s => s.BottomLeft).Returns(bottomLeft);
+			square.Setup(s => s.BottomRight).Returns(bottomRight);
+			square.Setup(s => s.TopLeft).Returns(topLeft);
+			square.Setup(s => s.TopRight).Returns(topRight);
+
+			return mask.IsMasked(new AGSPoint (x, y), square.Object, scaleX, scaleY);
+		}
+
+		[TestCase(100,100, 0,0, Result=true)]
+		[TestCase(100,100, 1,1, Result=true)]
+		[TestCase(100,100, 5,5, Result=false)]
+		[TestCase(100,100, 99,99, Result=false)]
+
+		[TestCase(100,100, 0,0, 1,1, Result=true)]
+		[TestCase(100,100, 1,1, 1,1, Result=true)]
+		[TestCase(100,100, 5,5, 1,1, Result=false)]
+		[TestCase(100,100, 99,99, 1,1, Result=false)]
+
+		[TestCase(100,100, 5,5, 5,5, Result=true)]
+		[TestCase(100,100, 5,5, 5,5, 4,4, 7,7, Result=true)]
+		[TestCase(100,100, 5,5, 6,6, 4,4, 7,7, Result=false)]
+		public bool ApplyToMaskTest(int width, int height, int xToTest, int yToTest, params int[] pointsInMask)
+		{
+			bool[][] targetMask = getArray(width, height, 0,0, 1,1, 2,2);
+			bool[][] sourceMask = getArray(width, height, pointsInMask);
+			AGSMask mask = new AGSMask (sourceMask, null);
+			mask.ApplyToMask(targetMask);
+
+			return targetMask[xToTest][yToTest];
+		}
+
+		private bool[][] getArray(int width, int height, params int[] pointsInMask)
+		{
 			bool[][] array = new bool[width][];
 
 			for (int x = 0; x < width; x++) array[x] = new bool[height];
@@ -28,13 +140,7 @@ namespace Tests
 				int y = pointsInMask[pointIndex + 1];
 				array[x][y] = true;
 			}
-
-			AGSMask mask = new AGSMask(array, null);
-
-			Assert.AreEqual(expectedMinX, mask.MinX);
-			Assert.AreEqual(expectedMaxX, mask.MaxX);
-			Assert.AreEqual(expectedMinY, mask.MinY);
-			Assert.AreEqual(expectedMaxY, mask.MaxY);
+			return array;
 		}
 	}
 }
