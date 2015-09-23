7d0d09f15bf5cae291077806fb9d4668d40f5039
Tzach Shabtay <tzachshabtay@gmail.com>
2015-09-23 14:19:53 -0400

    Mask Loader- use fast bitmap access

--- Engine/Rooms/Masks/AGSMaskLoader.cs
+++ Engine/Rooms/Masks/AGSMaskLoader.cs
@@ -27,30 +27,6 @@ namespace AGS.Engine
 		public IMask Load(Bitmap image, bool transparentMeansMasked = false, 
 			Color? debugDrawColor = null, string saveMaskToFile = null)
 		{
-			//Get the bitmap data
-			var bitmapData = image.LockBits (
-				new Rectangle (0, 0, image.Width, image.Height),
-				ImageLockMode.ReadWrite, 
-				image.PixelFormat
-			);
-
-			//Initialize an array for all the image data
-			byte[] imageBytes = new byte[bitmapData.Stride * image.Height];
-
-			//Copy the bitmap data to the local array
-			//todo: This might require some more work (casting to int* to avoid little endianess issues),
-			//see here: http://stackoverflow.com/questions/8104461/pixelformat-format32bppargb-seems-to-have-wrong-byte-order
-			Marshal.Copy (bitmapData.Scan0, imageBytes, 0, imageBytes.Length);
-
-			//Unlock the bitmap
-			image.UnlockBits(bitmapData);
-
-			//Find pixelsize
-			int pixelSize = Image.GetPixelFormatSize(image.PixelFormat);
-
-			// An example on how to use the pixels, lets make a copy
-			int x = 0;
-			int y = 0;
 			Bitmap debugMask = null;
 			FastBitmap debugMaskFast = null;
 			if (saveMaskToFile != null || debugDrawColor != null)
@@ -58,47 +34,34 @@ namespace AGS.Engine
 				debugMask = new Bitmap (image.Width, image.Height);
 				debugMaskFast = new FastBitmap (debugMask, ImageLockMode.WriteOnly, true);
 			}
-
+				
 			bool[][] mask = new bool[image.Width][];
 			Color drawColor = debugDrawColor.HasValue ? debugDrawColor.Value : Color.Black;
-			//Loop pixels
-			for(int i=0;i<imageBytes.Length;i+=pixelSize/8)
+
+			using (FastBitmap bitmapData = new FastBitmap (image, ImageLockMode.ReadOnly))
 			{
-				//Copy the bits into a local array
-				var pixelData = new byte[4];
-				Array.Copy(imageBytes,i,pixelData,0,4);
-
-				//Get the color of a pixel
-				//todo: add more formats, currently assuming Format32bppArgb
-				var pixelColor = Color.FromArgb(pixelData[3], pixelData [2], pixelData [1], pixelData [0]);
-
-				if (y < image.Height)
+				for (int x = 0; x < image.Width; x++)
 				{
-					bool masked = pixelColor.A == 255;
-					if (transparentMeansMasked)
-						masked = !masked;
-
-					if (mask[x] == null)
-						mask[x] = new bool[image.Height];
-					mask[x][image.Height - y - 1] = masked;
-
-					if (debugMask != null)
+					for (int y = 0; y < image.Height; y++)
 					{
-						debugMaskFast.SetPixel(x, y, masked ? drawColor : Color.Transparent);
+						var pixelColor = bitmapData.GetPixel(x, y);
+
+						bool masked = pixelColor.A == 255;
+						if (transparentMeansMasked)
+							masked = !masked;
+
+						if (mask[x] == null)
+							mask[x] = new bool[image.Height];
+						mask[x][image.Height - y - 1] = masked;
+
+						if (debugMask != null)
+						{
+							debugMaskFast.SetPixel(x, y, masked ? drawColor : Color.Transparent);
+						}
 					}
 				}
-
-
-				//Map the 1D array to (x,y)
-				x++;
-				if( x >= image.Width)
-				{
-					x=0;
-					y++;
-				}
-
 			}
-
+				
 			if (debugMask != null)
 				debugMaskFast.Dispose();
 
@@ -116,7 +79,7 @@ namespace AGS.Engine
 
 			return new AGSMask (mask, debugDraw);
 		}
-
+			
 		#endregion
 	}
 }
