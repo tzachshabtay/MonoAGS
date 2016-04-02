83b9bbe13bd6d4364ad68bc822fc9cce83fe4a6c
tzachshabtay <tzachshabtay@gmail.com>
2016-04-02 17:00:01 -0400

    Fix for windows: custom font was disposed PrivateFontCollection was not kept in memory, causing the loaded font to be disposed, causing a later crash.

--- Engine/UI/Text/AGSFontLoader.cs
+++ Engine/UI/Text/AGSFontLoader.cs
@@ -3,19 +3,29 @@ using AGS.API;
 using System.Drawing;
 using System.Runtime.InteropServices;
 using System.Drawing.Text;
+using System.Collections.Generic;
 
 namespace AGS.Engine
 {
 	public class AGSFontLoader
 	{
 		private IResourceLoader _resources;
+        private PrivateFontCollection _fontCollection;
+        private Dictionary<string, FontFamily> _families;
 
 		public AGSFontLoader(IResourceLoader resources)
 		{
 			_resources = resources;
+            _fontCollection = new PrivateFontCollection();
+            _families = new Dictionary<string, FontFamily>();
 		}
 
-		public FontFamily LoadFontFamily(string path)
+        public FontFamily LoadFontFamily(string path)
+        {
+            return _families.GetOrAdd(path, () => loadFontFamily(path));
+        }
+
+		private FontFamily loadFontFamily(string path)
 		{
 			IResource resource = _resources.LoadResource(path);
 
@@ -27,12 +37,11 @@ namespace AGS.Engine
 			Marshal.Copy(buffer, 0, fontPtr, buffer.Length);
 
 			//todo: this only works on windows: http://stackoverflow.com/questions/32406859/privatefontcollection-in-mono-3-2-8-on-linux-ubuntu-14-04-1
-			PrivateFontCollection fontCollection = new PrivateFontCollection();
-			fontCollection.AddMemoryFont(fontPtr, buffer.Length);
+			_fontCollection.AddMemoryFont(fontPtr, buffer.Length);
 
 			Marshal.FreeCoTaskMem(fontPtr);
 
-			return fontCollection.Families[0];
+			return _fontCollection.Families[_fontCollection.Families.Length - 1];
 			/*var handle = GCHandle.Alloc(buffer, GCHandleType.Pinned);
 			/*try 
 			{
