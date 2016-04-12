7aab994a0ffe72a0b8f6044f185db900ca2360a7
Tzach Shabtay <tzachshabtay@gmail.com>
2016-04-12 15:09:15 -0400

    AGS API converted to PCL
    AGS API was converted to PCL (portable class library) so it is now
    fully cross platform.

--- AGS.API/Properties/AssemblyInfo.cs
+++ AGS.API/Properties/AssemblyInfo.cs
@@ -1,9 +1,10 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("API")]
+[assembly: AssemblyTitle("AGS.API")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
