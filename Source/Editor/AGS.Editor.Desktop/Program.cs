9373fb0d61ffde6f71cf39fbf70dd42a2c9b77a7
tzachshabtay <tzachshabtay@gmail.com>
2016-08-20 10:36:28 -0400

    Recreated the Editor.Desktop project with Xamarin Studio
    With the hope that the build will pass

--- Source/Editor/AGS.Editor.Desktop/Program.cs
+++ Source/Editor/AGS.Editor.Desktop/Program.cs
@@ -1,15 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AGS.Editor.Desktop
 {
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-        }
-    }
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{			
+		}
+	}
 }
