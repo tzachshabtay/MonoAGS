cf7394a3a452fcf0d7f03134c5dd2a9a3e38fc40
tzachshabtay <tzachshabtay@gmail.com>
2016-08-19 20:43:12 -0400

    Created the basic skeleton for the editor project (minus vs extension for now)
    Resolves #30

---
+++ Source/Editor/AGS.Editor.Desktop/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AGS.Editor.Desktop
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
