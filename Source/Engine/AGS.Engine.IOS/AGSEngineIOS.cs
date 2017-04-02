d2d6b727218aac3e6eb363274c4c377780b0c37d
Tzach Shabtay <tzachshabtay@gmail.com>
2017-04-01 22:37:50 -0400

    Fix Autofac crash due to linker stripping out a function used by reflection only

--- Source/Engine/AGS.Engine.IOS/AGSEngineIOS.cs
+++ Source/Engine/AGS.Engine.IOS/AGSEngineIOS.cs
@@ -9,6 +9,16 @@ namespace AGS.Engine.IOS
 
         public static void Init()
         {
+            // On IOS, when the mono linker is enabled (and it's enabled by default) it strips out all parts of
+            // the framework which are not in use. The linker does not support reflection, however, therefore it
+            // does not recognize the call to 'GetRuntimeMethod(nameof(Convert.ChangeType)' (which is called from
+            // a method in this Autofac in ConstructorParameterBinding class, ConvertPrimitiveType method) 
+            // as using the 'Convert.ChangeType' method and strips it away unless it's
+            // explicitly used somewhere else, crashing Autofac if that method is called.
+            // Therefore, by explicitly calling it here, we're playing nicely with the linker and making sure
+            // Autofac works on IOS.
+            Convert.ChangeType((object)1234f, typeof(float));
+
             OpenTK.Toolkit.Init();
 
             var device = new IOSDevice(_assembly);
