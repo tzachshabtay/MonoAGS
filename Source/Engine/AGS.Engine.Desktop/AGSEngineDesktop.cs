4ce89eee9feba10620278c6e3c96b3ada603b779
tzachshabtay <tzachshabtay@gmail.com>
2017-04-22 21:57:46 -0400

    Add OpenAL Soft dll
    This will ensure proper audio even if OpenAL is not installed on the
    windows machine.
    Fixes #138

--- Source/Engine/AGS.Engine.Desktop/AGSEngineDesktop.cs
+++ Source/Engine/AGS.Engine.Desktop/AGSEngineDesktop.cs
@@ -10,6 +10,7 @@ namespace AGS.Engine.Desktop
 		public static void Init()
 		{                        
 			OpenTK.Toolkit.Init();
+            WindowsOpenALSoftLoader.Load();
 			string currentDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location).Replace(".Desktop", "");
 			Directory.CreateDirectory(currentDir);
 			Environment.CurrentDirectory = currentDir;
