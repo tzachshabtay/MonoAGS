e1bb3ed4d5a0e98239943ef37a49ce527dcbcdde
Tzach Shabtay <tzachshabtay@gmail.com>
2017-04-28 22:41:16 -0400

    Add API docs to components framework

--- Source/AGS.API/ComponentsFramework/RequiredComponentAttribute.cs
+++ Source/AGS.API/ComponentsFramework/RequiredComponentAttribute.cs
@@ -2,6 +2,14 @@
 
 namespace AGS.API
 {
+    /// <summary>
+    /// A decorator for components, to hint that this component requires other components
+    /// in order to function.
+    /// For example, a walk component needs a translate (x,y,z) component to move x and y when the character
+    /// moves. The walk component also needs an animation component to animate the walk.
+    /// So a walk component will have a required component attribute with translate compnent, and another 
+    /// attribute with the animation component.
+    /// </summary>
 	[AttributeUsage(AttributeTargets.Interface, AllowMultiple = true)]
 	public class RequiredComponentAttribute : Attribute
 	{
@@ -11,8 +19,16 @@ namespace AGS.API
 			Mandatory = true;
 		}
 
+        /// <summary>
+        /// Gets the component.
+        /// </summary>
+        /// <value>The component.</value>
 		public Type Component { get; private set; }
 
+        /// <summary>
+        /// Gets a value indicating whether the component is mandatory for the current component to function.
+        /// </summary>
+        /// <value><c>true</c> if mandatory; otherwise, <c>false</c>.</value>
 		public bool Mandatory { get; private set; }
 	}
 }
