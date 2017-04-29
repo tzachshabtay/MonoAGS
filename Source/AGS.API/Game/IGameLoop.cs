f183941dddb7c6fcfa05e68c3f068d8497099076
Tzach Shabtay <tzachshabtay@gmail.com>
2017-04-29 15:12:04 -0400

    Add API docs for the game

--- Source/AGS.API/Game/IGameLoop.cs
+++ Source/AGS.API/Game/IGameLoop.cs
@@ -2,8 +2,16 @@
 
 namespace AGS.API
 {
+    /// <summary>
+    /// The game loop, which updates the game state on every tick.
+    /// </summary>
     public interface IGameLoop
 	{
+        /// <summary>
+        /// Updates the state (this trigger animation moves, walking, etc). 
+        /// This is called repeatedly by the engine, and usually should not be used by the user.
+        /// </summary>
+        /// <returns>The update task.</returns>
 		Task UpdateAsync();
 	}
 }
--- Source/AGS.API/Game/Settings/IRuntimeSettings.cs
+++ Source/AGS.API/Game/Settings/IRuntimeSettings.cs
@@ -1,15 +1,53 @@
 ﻿using System;
 namespace AGS.API
 {
+    /// <summary>
+    /// Game settings.
+    /// </summary>
     public interface IRuntimeSettings : IGameSettings
     {
+        /// <summary>
+        /// Gets the title (the name of the game): would appear on the game window's title bar.
+        /// </summary>
+        /// <value>The title.</value>
         new string Title { get; set; }
+
+        /// <summary>
+        /// Gets the size of the window (the size of the actual window on screen).
+        /// </summary>
+        /// <value>The size of the window.</value>
         new Size WindowSize { get; set; }
+
+        /// <summary>
+        /// If the game is configured to preserve the aspect ratio, then in case the window is resized
+        /// and the aspect ratio is changed, the screen will be letterboxed or pillarboxed so the
+        /// aspect ratio for the actual content will remain the same  
+        /// </summary>
         new bool PreserveAspectRatio { get; set; }
+
+        /// <summary>
+        /// Gets the state of the window (minimized/maximized/etc).
+        /// </summary>
+        /// <value>The state of the window.</value>
         new WindowState WindowState { get; set; }
+
+        /// <summary>
+        /// Gets the style of the window's border.
+        /// </summary>
+        /// <value>The window border.</value>
         new WindowBorder WindowBorder { get; set; }
+
+        /// <summary>
+        /// Gets the vsync mode (synchronization of the frame update rate with screen refresh rate to prevent tearing).
+        /// To read about how this works: https://hardforum.com/threads/how-vsync-works-and-why-people-loathe-it.928593/
+        /// </summary>
+        /// <value>The vsync.</value>
         new VsyncMode Vsync { get; set; }
 
+        /// <summary>
+        /// Resets the viewport. This is called by the engine when the window is resized and the viewport needs to be re-adjusted.
+        /// You can call it if you changed a setting which requires resetting the window (like preserver aspect ratio).
+        /// </summary>
         void ResetViewport();
     }
 }
