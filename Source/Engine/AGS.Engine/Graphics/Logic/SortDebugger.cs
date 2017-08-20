4c3a4e72336a6018a559ae5583c6dc8b1298cba3
Tzach Shabtay <tzachshabtay@gmail.com>
2017-08-19 23:13:36 -0400

    Sort debugger- remove code that was checked in by mistake

--- Source/Engine/AGS.Engine/Graphics/Logic/SortDebugger.cs
+++ Source/Engine/AGS.Engine/Graphics/Logic/SortDebugger.cs
@@ -49,12 +49,12 @@ namespace AGS.Engine
 			bool renderDebug = _renderDebug;
 			_behindThis = null;
 
-			Dictionary<string, List<Tuple<string, string>>> map = new Dictionary<string, List<Tuple<string, string>>>();
+			Dictionary<string, List<string>> map = new Dictionary<string, List<string>>();
 			const string sortWord = "Sort ";
 			const string backwardsWord = "backwards ";
 			foreach (var obj in displayList)
 			{
-				foreach (var property in obj.Properties.Strings.AllProperties())
+                foreach (var property in obj.Properties.Ints.AllProperties())
 				{
 					if (!property.Key.Contains(sortWord)) continue;
 					if (renderDebug && property.Key.Contains(backwardsWord)) continue;
@@ -63,9 +63,8 @@ namespace AGS.Engine
 					string comparedId = property.Key.Substring(renderDebug ? property.Key.IndexOf(sortWord) + sortWord.Length :
 						property.Key.IndexOf(backwardsWord) + backwardsWord.Length);
 
-					string[] tokens = property.Value.Split(',');
-					if (int.Parse(tokens[0]) > 0) map.GetOrAdd(comparedId, () => new List<Tuple<string, string>>()).Add(new Tuple<string, string>(obj.ID, tokens[1]));
-					else map.GetOrAdd(obj.ID, () => new List<Tuple<string, string>>()).Add(new Tuple<string, string>(comparedId, tokens[1]));
+                    if (property.Value > 0) map.GetOrAdd(comparedId, () => new List<string>()).Add(obj.ID);
+                    else map.GetOrAdd(obj.ID, () => new List<string>()).Add(comparedId);
 				}
 			}
 			string chain = getChain(whyIsThis, behindThis, map, new HashSet<string>());
@@ -73,16 +72,16 @@ namespace AGS.Engine
 				string.Format("{0}{1}", whyIsThis, chain));
         }
 
-        private static string getChain(string from, string to, Dictionary<string, List<Tuple<string, string>>> map, HashSet<string> checkedChain)
+        private static string getChain(string from, string to, Dictionary<string, List<string>> map, HashSet<string> checkedChain)
         {
-            List<Tuple<string, string>> mapped;
+            List<string> mapped;
             if (!map.TryGetValue(from, out mapped)) return null;
-            foreach (Tuple<string, string> item in mapped)
+            foreach (string item in mapped)
             {
-                if (item.Item1 == to) return string.Format("-->{0}({1})", to, item.Item2);
-                if (!checkedChain.Add(item.Item1)) continue;
-                string chain = getChain(item.Item1, to, map, checkedChain);
-                if (chain != null) return string.Format("-->{0}({1}){2}", item.Item1, item.Item2, chain);
+                if (item == to) return string.Format("-->{0}", to);
+                if (!checkedChain.Add(item)) continue;
+                string chain = getChain(item, to, map, checkedChain);
+                if (chain != null) return string.Format("-->{0}{1}", item, chain);
             }
             return null;
         }
