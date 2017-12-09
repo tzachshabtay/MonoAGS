7d84de2a93b0b65964cce07a7737a1ebf7115129
Tzach Shabtay <tzachshabtay@gmail.com>
2017-12-09 15:21:25 -0500

    Fix tests

--- Source/Tests/Mocks.cs
+++ Source/Tests/Mocks.cs
@@ -224,9 +224,9 @@ namespace Tests
 			{
 				_roomEvents = new Mock<IRoomEvents> ();
 				_roomEvents.Setup(r => r.OnAfterFadeIn).Returns(new Mock<IEvent> ().Object);
-				_roomEvents.Setup(r => r.OnAfterFadeOut).Returns(new Mock<IEvent> ().Object);
-				_roomEvents.Setup(r => r.OnBeforeFadeIn).Returns(new Mock<IEvent> ().Object);
-				_roomEvents.Setup(r => r.OnBeforeFadeOut).Returns(new Mock<IEvent> ().Object);
+				_roomEvents.Setup(r => r.OnAfterFadeOut).Returns(new Mock<IBlockingEvent> ().Object);
+				_roomEvents.Setup(r => r.OnBeforeFadeIn).Returns(new Mock<IBlockingEvent> ().Object);
+				_roomEvents.Setup(r => r.OnBeforeFadeOut).Returns(new Mock<IBlockingEvent> ().Object);
 			}
 			return _roomEvents;
 		}
