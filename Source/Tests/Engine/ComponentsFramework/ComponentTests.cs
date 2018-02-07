6642987b13f266dcc7a6724d78268309ccdb7655
Tzach Shabtay <tzachshabtay@gmail.com>
2018-02-06 23:08:21 -0500

    Bug fix- memory leak in animation subscriber
    Because AnimationSubscriber was a struct, event subscription would
    create a copy of the struct, and unsubscription silently failed. Fixed
    by making animation subscriber a class.

---
+++ Source/Tests/Engine/ComponentsFramework/ComponentTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel;
+using AGS.API;
+using AGS.Engine;
+using Moq;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class ComponentTests
+    {
+        [Test]
+        public void ComponentPropertyChangedSubscription()
+        {
+            AGSSprite component = new AGSSprite(Mocks.GetResolver(), new Mock<IMaskLoader>().Object);
+            Assert.AreEqual(0, component.GetPropertyChangedSubscriberCount());
+
+            component.PropertyChanged += onComponentPropertyChanged;
+            Assert.AreEqual(1, component.GetPropertyChangedSubscriberCount());
+
+            component.PropertyChanged -= onComponentPropertyChanged;
+            Assert.AreEqual(0, component.GetPropertyChangedSubscriberCount());
+        }
+
+        private void onComponentPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+        }
+    }
+}
