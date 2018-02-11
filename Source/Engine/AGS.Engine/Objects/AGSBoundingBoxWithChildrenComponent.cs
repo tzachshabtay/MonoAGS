cb6cf4405d52b1c95754a5de3ca98025a508c0a5
Tzach Shabtay <tzachshabtay@gmail.com>
2018-02-11 15:56:41 -0500

    Fix debug printout

--- Source/Engine/AGS.Engine/Objects/AGSBoundingBoxWithChildrenComponent.cs
+++ Source/Engine/AGS.Engine/Objects/AGSBoundingBoxWithChildrenComponent.cs
@@ -148,7 +148,7 @@ namespace AGS.Engine
             _preCropBoundingBoxWithChildren = getBoundingBox(_tree, _boundingBox, boxes => boxes.PreCropRenderBox, DebugPrintouts ? $"Box Pre Crop ({_entity.ID})" : null);
             if (DebugPrintouts)
             {
-                Debug.WriteLine($"Pre crop for {_entity.ID}: {_preUnlockBoundingBox.ToString()}");
+                Debug.WriteLine($"Pre crop for {_entity.ID}: {_preCropBoundingBoxWithChildren.ToString()}");
             }
             return (!lastBox.Equals(BoundingBoxWithChildren) || !lastPreBox.Equals(PreCropBoundingBoxWithChildren));
         }
