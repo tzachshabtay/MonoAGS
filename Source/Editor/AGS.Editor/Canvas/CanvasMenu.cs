32702209a77e1cf978bd3ecc954126b9c185172d
Tzach Shabtay <tzachshabtay@gmail.com>
2018-07-03 16:59:28 -0400

    Support multi-nested menus
    Refactored into menu and menu item classes, and added support for
    multi-level nested menus.

--- Source/Editor/AGS.Editor/Canvas/CanvasMenu.cs
+++ Source/Editor/AGS.Editor/Canvas/CanvasMenu.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Threading.Tasks;
 using AGS.API;
-using AGS.Engine;
 
 namespace AGS.Editor
 {
@@ -18,112 +16,43 @@ namespace AGS.Editor
 
         public void Load()
         {
-            var layer = new AGSRenderLayer(AGSLayers.UI.Z - 1);
-            var menu = _factory.UI.GetListBox("CanvasMenu", layer, isVisible: false, withScrollBars: false);
-            const float width = 100f;
-
-            menu.ContentsPanel.Tint = GameViewColors.Menu;
-
-            bool isHovered = false;
-            var (presetsMenu, numHovered) = getPresetsListbox(layer, () => isHovered);
-
-            var whiteBrush = _factory.Graphics.Brushes.LoadSolidBrush(Colors.White);
-            var textConfig = new AGSTextConfig(whiteBrush, autoFit: AutoFit.TextShouldFitLabel);
-            var idle = new ButtonAnimation(null, textConfig, Colors.Transparent);
-            var hovered = new ButtonAnimation(null, textConfig, GameViewColors.HoveredMenuItem);
-            menu.ListboxComponent.ListItemFactory = (string text) =>
-            {
-                var button = _factory.UI.GetButton($"MenuItem_{text}", idle, hovered, hovered,
-                                                   0f, 0f, width: width, height: 25f, text: text);
-                button.Pivot = new PointF(0f, 1f);
-                button.RenderLayer = layer;
-
-                var subMenuIcon = _factory.UI.GetLabel($"{text}_SubMenuIcon", "", 25f, 25f, 80f, 0f, button, FontIcons.IconConfig);
-                subMenuIcon.Text = FontIcons.SubMenu;
-                subMenuIcon.RenderLayer = button.RenderLayer;
-
-                button.MouseEnter.Subscribe(() =>
-                {
-                    isHovered = true;
-                    presetsMenu.ContentsPanel.Visible = true;
-                });
-
-                button.MouseLeave.Subscribe(async () =>
-                {
-                    isHovered = false;
-                    await Task.Delay(100);
-                    if (numHovered() <= 0)
-                    {
-                        presetsMenu.ContentsPanel.Visible = false;
-                    }
-                });
-
-                return button;
-            };
-
-            menu.ListboxComponent.Items.Add(new AGSStringItem { Text = "Create" });
-
-            menu.ContentsPanel.Pivot = (0f, 1f);
+            Action noop = () => { };
+            Menu guisMenu = new Menu("GuisMenu", 180f,
+                                     new MenuItem("Button", noop),
+                                     new MenuItem("Label", noop),
+                                     new MenuItem("ComboBox", noop),
+                                     new MenuItem("TextBox", noop),
+                                     new MenuItem("Inventory Window", noop),
+                                     new MenuItem("Checkbox", noop),
+                                     new MenuItem("Listbox", noop),
+                                     new MenuItem("Panel", noop),
+                                     new MenuItem("Slider", noop));
+            Menu areasMenu = new Menu("AreasMenu", 150f,
+                                      new MenuItem("Walkable Area", noop),
+                                      new MenuItem("Walk-Behind", noop),
+                                      new MenuItem("Scaling Area", noop),
+                                      new MenuItem("Zoom Area", noop),
+                                      new MenuItem("Empty Area", noop));
+            Menu presetsMenu = new Menu("PresetsMenu", 100f,
+                                        new MenuItem("Object", noop), 
+                                        new MenuItem("Character", noop),
+                                        new MenuItem("GUIs", guisMenu),
+                                        new MenuItem("Areas", areasMenu));
+            Menu topMenu = new Menu("CanvasMenu", 100f, new MenuItem("Create", presetsMenu));
+            topMenu.Load(_factory);
 
             _input.MouseUp.Subscribe((MouseButtonEventArgs args) => 
             {
                 if (args.Button == MouseButton.Right)
                 {
-                    menu.ContentsPanel.Position = (args.MousePosition.XMainViewport, args.MousePosition.YMainViewport);
-                    presetsMenu.ContentsPanel.Position = (args.MousePosition.XMainViewport + width + 3f, Math.Max(100f, args.MousePosition.YMainViewport));
-                    menu.ContentsPanel.Visible = true;
+                    topMenu.Position = (args.MousePosition.XMainViewport, args.MousePosition.YMainViewport);
+                    topMenu.Visible = true;
                 }
                 else if (args.ClickedEntity == null) 
-                { 
-                    menu.ContentsPanel.Visible = false; 
-                    presetsMenu.ContentsPanel.Visible = false; 
+                {
+                    topMenu.Visible = false;
                 } 
             });
-        }
-
-        private (IListbox, Func<int>) getPresetsListbox(IRenderLayer layer, Func<bool> isParentHovered)
-        {
-            var menu = _factory.UI.GetListBox("PresetsMenu", layer, isVisible: false, withScrollBars: false);
-
-            menu.ContentsPanel.Tint = GameViewColors.Menu;
-
-            var whiteBrush = _factory.Graphics.Brushes.LoadSolidBrush(Colors.White);
-            var textConfig = new AGSTextConfig(whiteBrush, autoFit: AutoFit.TextShouldFitLabel);
-            var idle = new ButtonAnimation(null, textConfig, Colors.Transparent);
-            var hovered = new ButtonAnimation(null, textConfig, GameViewColors.HoveredMenuItem);
-            int numHovered = 0;
-            menu.ListboxComponent.ListItemFactory = (string text) =>
-            {
-                var button = _factory.UI.GetButton($"MenuItem_{text}", idle, hovered, hovered,
-                                                                 0f, 0f, width: 100f, height: 25f, text: text);
-                button.Pivot = new PointF(0f, 1f);
-                button.RenderLayer = layer;
-
-                button.MouseEnter.Subscribe(() =>
-                {
-                    numHovered++;
-                });
-
-                button.MouseLeave.Subscribe(async () =>
-                {
-                    numHovered--;
-                    await Task.Delay(100);
-                    if (numHovered <= 0 && !isParentHovered())
-                    {
-                        menu.ContentsPanel.Visible = false;
-                    }
-                });
-
-                return button;
-            };
-
-            menu.ListboxComponent.Items.Add(new AGSStringItem { Text = "Object" });
-            menu.ListboxComponent.Items.Add(new AGSStringItem { Text = "Character" });
-            menu.ListboxComponent.Items.Add(new AGSStringItem { Text = "Button" });
-            menu.ListboxComponent.Items.Add(new AGSStringItem { Text = "Label" });
-
-            menu.ContentsPanel.Pivot = (0f, 1f);
-            return (menu, () => numHovered);
         }
     }
 }
---
+++ Source/Editor/AGS.Editor/Components/Menu/Menu.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Linq;
+using AGS.API;
+using AGS.Engine;
+
+namespace AGS.Editor
+{
+    public class Menu
+    {
+        private MenuItem[] _menuItems;
+        private readonly string _id;
+        private IListbox _menu;
+        private readonly float _width;
+        private const float _height = 25f;
+
+        public Menu(string id, float width, params MenuItem[] menuItems)
+        {
+            _width = width;
+            _menuItems = menuItems;
+            foreach (var item in menuItems) item.ParentMenu = this;
+            _id = id;
+        }
+
+        public bool IsHovered => _menuItems.Any(m => m.IsHovered);
+
+        public MenuItem ParentMenuItem { get; set; }
+
+        public bool Visible 
+        { 
+            get => _menu.ContentsPanel.Visible; 
+            set
+            {
+                _menu.ContentsPanel.Visible = value;
+                if (!value)
+                {
+                    foreach (var item in _menuItems)
+                    {
+                        if (item.SubMenu != null) item.SubMenu.Visible = false;
+                    }
+                }
+            }
+        }
+
+        public (float x, float y) Position
+        {
+            set
+            {
+                float minY = _height * _menuItems.Length;
+                _menu.ContentsPanel.Position = (value.x, Math.Max(minY, value.y));
+                (float x, float y) = value;
+                for (int index = 0; index < _menuItems.Length; index++)
+                {
+                    var item = _menuItems[index];
+                    if (item.SubMenu != null) item.SubMenu.Position = (x + _width + 3f, y - index * _height);
+                }
+            }
+        }
+
+        public void Load(IGameFactory factory)
+        {
+            var layer = new AGSRenderLayer(AGSLayers.UI.Z - 1);
+            _menu = factory.UI.GetListBox(_id, layer, isVisible: false, withScrollBars: false);
+
+            _menu.ContentsPanel.Tint = GameViewColors.Menu;
+
+            var whiteBrush = factory.Graphics.Brushes.LoadSolidBrush(Colors.White);
+            var textConfig = new AGSTextConfig(whiteBrush, autoFit: AutoFit.TextShouldFitLabel);
+            var idle = new ButtonAnimation(null, textConfig, Colors.Transparent);
+            var hovered = new ButtonAnimation(null, textConfig, GameViewColors.HoveredMenuItem);
+
+            _menu.ListboxComponent.ListItemFactory = (string text) =>
+            {
+                var menuItem = _menuItems.First(m => m.Text == text);
+                return menuItem.GetControl(factory, layer, idle, hovered, _width, _height);
+            };
+
+            _menu.ListboxComponent.Items.AddRange(_menuItems.Cast<IStringItem>().ToList());
+            _menu.ContentsPanel.Pivot = (0f, 1f);
+
+            foreach (var item in _menuItems)
+            {
+                if (item.SubMenu != null) item.SubMenu.Load(factory);
+            }
+        }
+    }
+}
