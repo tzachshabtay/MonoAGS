d5c8fc7f2794fdbd6a293baf957f1d8049e3dd29
Tzach Shabtay <tzachshabtay@gmail.com>
2018-07-14 00:22:16 -0400

    Added a radio group functionality
    This allows grouping several checkboxes together (as radio buttons) so
    that when one is checked, all other buttons are unchecked automatically.

---
+++ Source/AGS.API/UI/Controls/Checkbox/IRadioGroup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace AGS.API
+{
+    /// <summary>
+    /// Used to group multiple checkboxes together to guarantee that only one will be checked at any given time.
+    /// </summary>
+    /// <seealso cref="ICheckboxComponent.RadioGroup"/>
+    public interface IRadioGroup : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Gets or sets the selected button.
+        /// Once a new button is selected, any existing checked button in the group will be unchecked.
+        /// </summary>
+        /// <value>The selected button.</value>
+        ICheckboxComponent SelectedButton { get; set; }
+    }
+}
