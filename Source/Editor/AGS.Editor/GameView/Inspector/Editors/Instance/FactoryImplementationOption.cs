8b7136d379704bd5ada54d94c4f72679e4e29485
Tzach Shabtay <tzachshabtay@gmail.com>
2018-08-31 00:03:22 -0400

    Editor- fix factory method name

--- Source/Editor/AGS.Editor/GameView/Inspector/Editors/Instance/FactoryImplementationOption.cs
+++ Source/Editor/AGS.Editor/GameView/Inspector/Editors/Instance/FactoryImplementationOption.cs
@@ -9,11 +9,13 @@ namespace AGS.Editor
     {
         private readonly object _factory;
         private readonly AGSEditor _editor;
+        private readonly string _methodName;
 
         public FactoryImplementationOption(HasFactoryAttribute attr, AGSEditor editor)
         {
             _editor = editor;
             _factory = FactoryProvider.GetFactory(attr.FactoryType, editor.Game);
+            _methodName = attr.MethodName;
             Name = attr.DisplayName ?? attr.MethodName.Humanize();
         }
 
@@ -22,7 +24,7 @@ namespace AGS.Editor
         public async Task<SelectEditor.ReturnValue> Create(IForm parentForm, string title)
         {
             var factoryWizard = new FactoryWizard(parentForm, title, _editor, null, null, null);
-            (object result, MethodModel model, MethodWizardAttribute attr) = await factoryWizard.RunMethod(_factory, Name);
+            (object result, MethodModel model, MethodWizardAttribute attr) = await factoryWizard.RunMethod(_factory, _methodName);
             return new SelectEditor.ReturnValue(result, model == null);
         }
     }
