7706efe18abee2b7f9ce0bad7ab8ed6cb10f8a64
Tzach Shabtay <tzachshabtay@gmail.com>
2019-01-01 09:33:59 -0500

    GL Matrices- remove unused public setters for properties

--- Source/Engine/AGS.Engine/Graphics/Logic/GLMatrices.cs
+++ Source/Engine/AGS.Engine/Graphics/Logic/GLMatrices.cs
@@ -1,12 +1,13 @@
-﻿using AGS.API;
+﻿using System.Diagnostics;
+using AGS.API;
 
 namespace AGS.Engine
 {
     public class GLMatrices : IGLMatrices
     {
-        public Matrix4 ModelMatrix { get; set; }
+        public Matrix4 ModelMatrix { get; private set; }
 
-        public Matrix4 ViewportMatrix { get; set; }
+        public Matrix4 ViewportMatrix { get; private set; }
 
         public GLMatrices SetMatrices(Matrix4 modelMatrix, Matrix4 viewportMatrix)
         {
