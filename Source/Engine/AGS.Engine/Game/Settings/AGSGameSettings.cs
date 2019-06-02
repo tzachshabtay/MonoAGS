5660e805beb22030e7c2a4b1a849a85251b1033f
Tzach Shabtay <tzachshabtay@gmail.com>
2019-06-01 23:11:38 -0400

    merge

--- Source/Engine/AGS.Engine/Game/Settings/AGSGameSettings.cs
+++ Source/Engine/AGS.Engine/Game/Settings/AGSGameSettings.cs
@@ -4,15 +4,9 @@ namespace AGS.Engine
 {
 	public class AGSGameSettings : IGameSettings
 	{
-<<<<<<< HEAD
 		public AGSGameSettings(string title, AGS.API.Size virtualResolution, WindowState windowState = WindowState.Maximized,
                AGS.API.Size? windowSize = null, VsyncMode vsync = VsyncMode.On, bool preserveAspectRatio = true,
                WindowBorder windowBorder = WindowBorder.Resizable, GraphicsBackend? backend = null)
-=======
-		public AGSGameSettings(string title, Size virtualResolution, WindowState windowState = WindowState.Maximized,
-               Size? windowSize = null, VsyncMode vsync = VsyncMode.Adaptive, bool preserveAspectRatio = true,
-               WindowBorder windowBorder = WindowBorder.Resizable)
->>>>>>> 9275f923b096bbb121ad033d1cd020cfe21cc5f4
 		{
             Title = title;
             VirtualResolution = virtualResolution;
@@ -21,14 +15,10 @@ namespace AGS.Engine
             Vsync = vsync;
             PreserveAspectRatio = preserveAspectRatio;
             WindowBorder = windowBorder;
-<<<<<<< HEAD
             Backend = backend ?? AGSGame.Device.GraphicsBackend.AutoDetect();
-            Defaults = new AGSDefaultsSettings();
-=======
             var fonts = new AGSDefaultFonts();
             var dialogs = new AGSDialogSettings(AGSGame.Device, fonts);
             Defaults = new AGSDefaultsSettings(fonts, dialogs);
->>>>>>> 9275f923b096bbb121ad033d1cd020cfe21cc5f4
 		}
 
         public string Title { get; }
